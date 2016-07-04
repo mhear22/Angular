6c975f09fdd06a0ed04bbb67ad9fa8fd8c1f3e7e
McKay <mhear22@gmail.com>
2016-07-04 20:25:24 +1000

    Added build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,39 @@
 var gulp = require('gulp');
-var webserver = require('gulp-webserver')
+var webserver = require('gulp-webserver');
+var del = require('del');
 
-gulp.task('default', function () {
-	console.log("logged");
-});
+gulp.task('release',['build','minify','webserver']);
+gulp.task('default',['build','webserver']);
 
 gulp.task('webserver', function () {
-	gulp.src('app')
+	gulp.src('./')
 		.pipe(webserver({
 			livereload: true,
+			//path: '/dev',
+			directoryListing: {
+				enable: true,
+				path:"./dev"
+			},
 			open: true,
 			host: 'localhost',
-			port: 8080
+			port: 8080,
+			fallback: 'index.html'
 		}))
+})
+
+gulp.task('build',['clean','move'], function () {
+});
+
+gulp.task('move', function () {
+	gulp
+		.src(['src/**/*'])
+		.pipe(gulp.dest('dev'));
+});
+
+gulp.task('clean', function () {
+	del('dev/**/*');
+});
+
+gulp.task('minify', function () {
+	
 })
