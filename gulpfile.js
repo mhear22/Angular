b543faeaadec391612674f995f0ae57da1b5be29
McKay <mhear22@gmail.com>
2016-07-06 21:14:01 +1000

    added injection to develop

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,24 @@
 var	gulp 		= require('gulp'),
+	gulpUtil	= require('gulp-Util'),
 	webserver 	= require('gulp-webserver'),
 	del 		= require('del'),
 	concat 		= require('gulp-concat'),
 	uglify 		= require('gulp-uglify'),
-	inject	 	= require('gulp-inject');
-
+	inject	 	= require('gulp-inject'),
+	runSequ		= require('run-sequence');
 
 gulp
-.task('release',['build','minify'])
-.task('default',['build'])
-.task('run', ['build', 'webserver'])
-.task('build',['clean','move', 'inject'])
+.task('default',['build:develop'])
+.task('run', ['build:develop', 'webserver'])
+
+.task('build:release', function () {
+	runSequ('clean:release','move:release','minify','inject:release')
+})
+
+.task('build:develop', function () {
+	runSequ('clean:develop','move:develop','inject:develop');
+})
+
 
 .task('webserver', function () {
 	gulp.src('./')
@@ -27,27 +35,44 @@ gulp
 		}));
 })
 
-.task('move', function () {
-	gulp
+.task('move:develop', function () {
+	return gulp
 		.src(['src/**/*'])
 		.pipe(gulp.dest('dev'));
-
 })
 
-.task('inject', function () {
+.task('move:release', function () {
+	return gulp
+		.src(['src/**/*.css'],['src/**/*.html'])
+		.pipe(gulp.dest('dist'));
+})
+
+.task('inject:develop', function () {
 	var target = gulp.src('./dev/index.html');
-	var source = gulp.src(['./src/**/*.js'], {read: false});
+	var source = gulp.src(['./dev/**/*.js', './dev/**/*.css'], {read: false});
 	return target
-		.pipe(inject(source));
-	
+		.pipe(inject(source))
+		.pipe(gulp.dest('dev'));
 })
 
-.task('clean', function () {
-	del('dev/**/*');
+.task('inject:release', function () {
+	var target = gulp.src('./dist/index.html');
+	var source = gulp.src(['./dist/**/*.js', './dist/**/*.css'], {read: false});
+	return target
+		.pipe(inject(source))
+		.pipe(gulp.dest('dist'));
+})
+
+.task('clean:develop', function () {
+	return del('dev/**/*');
+})
+
+.task('clean:release', function () {
+	return del('dist/**/*');
 })
 
 .task('minify', function () {
-	gulp.src('src/**/*.js')
+	return gulp.src('dist/**/*.js')
 		.pipe(concat('a.js'))
 		.pipe(uglify())
 		.pipe(gulp.dest('dist'));
