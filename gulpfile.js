7a62b4ee872b6e6682c2203f9d9ce07679545008
McKay Heares <rosanneheares@Rosannes-MacBook-Pro.local>
2016-07-23 12:46:27 +1000

    release compiles correcttly

--- gulpfile.js
+++ gulpfile.js
@@ -11,26 +11,16 @@ var	gulp 		= require('gulp'),
 	project 	= tsc.createProject("tsconfig.json");
 
 gulp
-.task('default',['build:develop'])
+.task('default',['develop'])
 .task('run', ['build:develop', 'webserver'])
 
-.task('build:release', function () {
-	runSequ('clean:release','compile:release','move:release',['minify:js', 'minify:css'],'inject:release')
+
+///Release
+.task('release', function () {
+	return runSequ('inject:release')
 })
 
-.task('build:develop', function () {
-	runSequ('clean:develop','compile:develop','move:develop','depend','inject:develop');
-})
-
-.task('compile:develop', function () {
-	return project
-		.src()
-		.pipe(tsc(project))
-		.js
-		.pipe(gulp.dest('dev'))
-})
-
-.task('compile:develop', function () {
+.task('compile:release',['clean:release'], function () {
 	return project
 		.src()
 		.pipe(tsc(project))
@@ -38,6 +28,83 @@ gulp
 		.pipe(gulp.dest('dist'))
 })
 
+.task('move:release',['clean:release'], function () {
+	return gulp
+		.src(['./src/**/*.html'])
+		.pipe(gulp.dest('dist'));
+})
+
+.task('inject:release', ['compile:release', 'move:release','minify:js','minify:css', 'cleanjs:release'],function () {
+	var target = gulp.src('./dist/index.html');
+	var source = gulp.src(['./dist/**/*.js', './dist/**/*.css'], {read: false});
+	return target
+		.pipe(inject(source,{relative: true}))
+		.pipe(gulp.dest('./dist'));
+})
+
+.task('clean:release', function () {
+	return del('./dist/**/*');
+})
+
+.task('cleanjs:release',['compile:release', 'minify:js'], function () {
+	return del(['./dist/**/*.js', '!./dist/a.js'])
+		.then(paths => {console.log(paths)});
+})
+
+.task('minify:js',['compile:release'] ,function () {
+	return gulp.src('./dist/**/*.js')
+		.pipe(concat('a.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('dist'));
+})
+
+.task('minify:css',['clean:release'] ,function () {
+	return gulp.src('./src/**/*.css')
+		.pipe(concat('c.css'))
+		.pipe(cleanCss())
+		.pipe(gulp.dest('dist'));
+})
+
+
+///Develop
+.task('develop', function () {
+	runSequ('inject:develop');
+})
+
+.task('inject:develop', ['move:develop', 'compile:develop', 'depend'], function () {
+	var target = gulp.src('./dev/index.html');
+	var source = gulp.src(['./dev/**/*.js', './dev/**/*.css'], {read: false});
+	return target
+		.pipe(inject(source,{relative: true}))
+		.pipe(gulp.dest('./dev'));
+})
+
+.task('clean:develop', function () {
+	del('./dev/**/*');
+})
+
+.task('compile:develop',['clean:develop'], function () {
+	return project
+		.src()
+		.pipe(tsc(project))
+		.js
+		.pipe(gulp.dest('dev'))
+})
+
+.task('move:develop',['clean:develop'], function () {
+	return gulp
+		.src(['./src/**/*.css', './src/**/*.html'])
+		.pipe(gulp.dest('dev'));
+})
+
+.task('depend',['move:develop', 'compile:develop'], function () {
+	return gulp
+		.src(['./node_modules/angular/angular.js'])
+		.pipe(gulp.dest('dev'))
+})
+
+
+///Universal
 .task('webserver', function () {
 	gulp.src('./dev')
 		.pipe(webserver({
@@ -46,60 +113,4 @@ gulp
 			host: 'localhost',
 			port: 8080,
 		}));
-})
-
-.task('move:develop', function () {
-	return gulp
-		.src(['./src/**/*'])
-		.pipe(gulp.dest('dev'));
-})
-
-.task('move:release', function () {
-	return gulp
-		.src(['./src/**/*.html'])
-		.pipe(gulp.dest('dist'));
-})
-
-.task('depend', function () {
-	return gulp
-		.src(['./node_modules/angular/angular.js'])
-		.pipe(gulp.dest('dev'))
-})
-
-.task('inject:develop', function () {
-	var target = gulp.src('./dev/index.html');
-	var source = gulp.src(['./dev/**/*.js', './dev/**/*.css'], {read: false});
-	return target
-		.pipe(inject(source,{relative: true}))
-		.pipe(gulp.dest('./dev'));
-})
-
-.task('inject:release', function () {
-	var target = gulp.src('./dist/index.html');
-	var source = gulp.src(['./dist/**/*.js', './dist/**/*.css'], {read: false});
-	return target
-		.pipe(inject(source,{relative: true}))
-		.pipe(gulp.dest('./dist'));
-})
-
-.task('clean:develop', function () {
-	del('./dev/**/*');
-})
-
-.task('clean:release', function () {
-	del('./dist/**/*');
-})
-
-.task('minify:js', function () {
-	return gulp.src('./src/**/*.js')
-		.pipe(concat('a.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('dist'));
-})
-
-.task('minify:css', function () {
-	return gulp.src('./src/**/*.css')
-		.pipe(concat('c.css'))
-		.pipe(cleanCss())
-		.pipe(gulp.dest('dist'));
 })
