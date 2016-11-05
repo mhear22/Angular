389e4de21ba67158492bd85ede74a47fa0ca5e7d
McKay Heares <rosanneheares@Rosannes-MacBook-Pro.local>
2016-11-05 12:51:52 +1000

    Uses hash location strategy now

--- src/module.ts
+++ src/module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { LoginService } from './Services/LoginService';
 import { AppComponent }  from './components/app';
 import { LoginForm } from './components/Login/Login';
@@ -11,7 +12,7 @@ import { routes } from './routes';
 @NgModule({
 	imports:		[ BrowserModule, HttpModule, RouterModule.forRoot(routes) ],
 	declarations: 	[ AppComponent, LoginForm, Dashboard ],
-	providers: 		[ LoginService ],
+	providers: 		[{ provide: LocationStrategy, useClass: HashLocationStrategy }],
 	bootstrap:		[ AppComponent ]
 })
 export class AppModule { }
