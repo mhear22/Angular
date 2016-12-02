6af34a9e69f4a46a3bb66fb4de1074d78b10e42b
McKay <mhear22@gmail.com>
2016-12-02 19:05:48 +1000

    updated spec

--- Tests/Signup.spec.js
+++ Tests/Signup.spec.js
@@ -1,6 +1,9 @@
 describe('Signup page behavior', function(){
-	it('should launch', function(){
+	fit('should launch', function(){
 		browser.get('http://localhost:8080/#/signup');
-		element(by.name('username')).sendKeys("testUsername");
+		element(by.name('username')).sendKeys("username");
+		element(by.name('pass')).sendKeys("1234test");
+		element(by.name('email')).sendKeys("test@email");
+		element(by.name('submit')).click();
 	});
 });
