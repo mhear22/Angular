e4574613a944e0497dfef392a8bba93f8eed2775
mhear22 <mhear22@gmail.com>
2017-01-28 21:25:00 +1000

    Login screen has user feedback

--- src/Parts/Login/Login.ts
+++ src/Parts/Login/Login.ts
@@ -11,15 +11,19 @@ import { LoginModel } from '../../Models/User/LoginModel';
 export class LoginForm {
 	username:string = "";
 	password:string = "";
-	
+	private IsLoading:boolean = false;
 	constructor(private _loginService: LoginService, private router: Router) { }
 	
 	public Login() {
+		this.IsLoading = true;
 		var model = new LoginModel();
 		model.Username = this.username;
 		model.Password = this.password;
 		this._loginService.Login(model).subscribe(result => {
+			this.IsLoading = false;
 			this.router.navigate(['/home']);
-		})
+		}, error => {
+			this.IsLoading = false;
+		});
 	}
 }
