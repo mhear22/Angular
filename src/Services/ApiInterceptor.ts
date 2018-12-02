1c6ae1a0ba42bd7510c0e407a4d78f45c2091da8
mhear22 <mhear22@gmail.com>
2018-12-01 22:50:18 -0500

    Redirect to login page on Unauth

--- src/Services/ApiInterceptor.ts
+++ src/Services/ApiInterceptor.ts
@@ -1,14 +1,21 @@
 import { Injectable } from "@angular/core";
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from "@angular/common/http";
+import { Observable, throwError } from "rxjs";
 import { ServiceBase } from "./ServiceBase";
+import { Router } from "@angular/router";
+import { catchError } from "rxjs/operators";
 
 @Injectable()
 export class ApiInterceptor implements HttpInterceptor {
+	constructor(private router:Router) { }
+	
 	intercept(req:HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 		var authReq = req.clone({
 			headers:req.headers.set("apikey", ServiceBase.ApiKey)
 		});
-		return next.handle(authReq);
+		return next.handle(authReq).pipe(catchError((err) => {
+			this.router.navigate(['/login']);
+			return throwError(err);
+		}));
 	}
 }
