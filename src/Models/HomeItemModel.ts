4c5d2d9b77fd2384e5b32b16a473c1ed63894b24
McKay <mhear22@gmail.com>
2019-01-16 11:18:47 +1000

    Use an array to generate menu

---
+++ src/Models/HomeItemModel.ts
@@ -0,0 +1,10 @@
+export class HomeItemModel {
+	public Name:string="";
+	public IconClass:string="";
+	public RequiresLogin:boolean = true;
+	public RequiresPlan:boolean = true;
+	public Allowed:boolean = true;
+	public Route?:string = null;
+	public Action?:any = null;
+	public Invert?:boolean = false;
+}
--- src/Parts/Menu/Menu.ts
+++ src/Parts/Menu/Menu.ts
@@ -3,6 +3,8 @@ import { Router } from '@angular/router';
 import { LoginService } from '../../Services/LoginService';
 import { ServiceBase } from '../../Services/ServiceBase';
 import { Observable } from 'rxjs';
+import { HomeItemModel } from 'src/Models/HomeItemModel';
+import { PaymentService } from 'src/Services/Api/Api';
 
 @Component({
 	selector: 'menu',
@@ -19,10 +21,109 @@ export class MenuBar {
 		});
 	}
 	
+	private Items:HomeItemModel[] = [
+		{
+			Name:"Home",
+			IconClass:"fa fa-home",
+			Route:"/home",
+			RequiresLogin:true,
+			RequiresPlan:true,
+			Allowed:true
+		},
+		{
+			Name:"Add Car",
+			IconClass:"fa fa-car",
+			Route:"/add-car",
+			Allowed:true,
+			RequiresLogin:true,
+			RequiresPlan:true,
+		},
+		{
+			Name:"Profile",
+			IconClass:"fa fa-user",
+			Route:"profile",
+			Allowed:true,
+			RequiresLogin:true,
+			RequiresPlan:false,
+		},
+		{
+			Name:"Settings",
+			IconClass:"fa fa-cog",
+			Route:"/settings",
+			Allowed:true,
+			RequiresLogin:true,
+			RequiresPlan:true,
+		},
+		{
+			Name:"Sign Out",
+			IconClass:"fa fa-sign-out",
+			Action:() => {
+				this.Logout();
+			},
+			Allowed:true,
+			RequiresLogin:true,
+			RequiresPlan:false,
+		},
+		{
+			Name:"Sign In",
+			IconClass:"fa fa-sign-in",
+			Route:"/login",
+			Allowed:true,
+			RequiresLogin:false,
+			RequiresPlan:false,
+			Invert:true
+		}
+	];
+	
+	public Check():void {
+		this.Items.forEach(x=> {
+			var result = null;
+			if(x.RequiresLogin && x.RequiresPlan) {
+				var IsLoggedIn = this.IsLoggedIn == true;
+				var IsSubscribed = this.IsSubscribed == true;
+				
+				result = IsLoggedIn && IsSubscribed;
+				
+				//result = !((!this.IsLoggedIn) || (!this.IsSubscribed));
+			}
+			else if(x.RequiresLogin) {
+				result = this.IsLoggedIn;
+			}
+			else if(x.RequiresPlan) {
+				
+			}
+			
+			if(result != null) {
+				if(x.Invert) {
+					x.Allowed = !result
+				}
+				else
+					x.Allowed = result;
+			}
+			else {
+				x.Allowed = true;
+			}
+			
+		});
+	}
+	
+	
 	IsLoggedIn: boolean = false; 
+	IsSubscribed: boolean = false;
 	
 	public Refresh():void {
 		this.IsLoggedIn = this.login.IsLoggedIn();
+		if(this.IsLoggedIn) {
+			this.login.GetCurrentUser().subscribe(x=> {
+				this.IsSubscribed = !!x.PlanNickname
+				this.Check();
+			},()=> {
+				this.Check();
+			});
+		}
+		else {
+			this.Check();
+		}
 	}
 	
 	public Logout() {
@@ -31,5 +132,6 @@ export class MenuBar {
 		}
 		catch (ex) { }
 		this.router.navigate(['/home']);
+		this.Refresh();
 	}
 }
