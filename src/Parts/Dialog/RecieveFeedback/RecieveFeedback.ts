1e3f42c70efeb54c72df8e445f14c65f505a244d
McKay <mhear22@gmail.com>
2019-04-12 21:19:07 +1000

    Feedback doesnt require being logged in

--- src/Parts/Dialog/RecieveFeedback/RecieveFeedback.ts
+++ src/Parts/Dialog/RecieveFeedback/RecieveFeedback.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MatDialogRef } from '@angular/material';
-import { FeedbackService } from 'src/Services/Api/Api';
+import { FeedbackService, FeedbackModel } from 'src/Services/Api/Api';
 import { LoginService } from 'src/Services/LoginService';
 
 @Component({
@@ -19,15 +19,19 @@ export class RecieveFeedbackDialog {
 	
 	save() {
 		this.Loading = true;
+		var info:FeedbackModel = {
+			Feedback: this.Message,
+			Url:window.location.href
+		};
+		
 		this.loginService.GetCurrentUser().subscribe(x=>{
-			this.feedbackService.sendFeedback({
-				Feedback: this.Message,
-				UserId: x.Id,
-				Url:window.location.href
-			}).subscribe(() => {
+			info.UserId = x.Id;
+			this.feedbackService.sendFeedback(info).subscribe(() => {
 				this.diaRef.close()
 			},() => { this.Loading = false; })
-		},() => { this.Loading = false; })
-		
+		}, () =>
+		this.feedbackService.sendFeedback(info).subscribe(()=> {
+			this.diaRef.close()
+		}))
 	}
 }
